8507fe82bb809c120704c600bb64daab97495e73
Donaji Coello <donnsdonns14@gmail.com>
2024-02-19 13:25:43 -0600

    typo: descripcion del ejercicio

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log ("Hola Ch38")
+console.log ("Hola Ch38");
+
+//funcion que envie un saludo con alert
+
+//alert("Hola mundo");
+function enviarSaludo(){
+    alert ("Hola mundo");
+}
+/**
+ * Pedir el nombre de la persona usando un prompt
+ * Mostrar el saludo y nombre de la persona con alert
+ */
+function tuNombre(){
+
+}
+/**function enviarSaludoPersona(){
+prompt("Dime tu nombre");
+return(nombre)
+}
+*/
+function enviarSaludoPersona() {
+    let mensaje = prompt ("Dime tu nombre");
+    alert("Hola ${mensaje}");
+}
